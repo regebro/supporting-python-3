4c05e79bcf8d4d2ff298b7e3d3b28286433710a5
Patrick Gerken <do3cc@patrick-gerken.de>
2015-02-03 10:53:11 +0100

    Make sure that virtualenv pythons are found first

--- runtests.sh
+++ runtests.sh
@@ -2,28 +2,60 @@
 PYTHON23=`which python23`
 PYTHON23=${PYTHON23:-`which python2.3`}
 PYTHON23=${PYTHON23:-/opt/python23/bin/python}
+
 PYTHON24=`which python24`
 PYTHON24=${PYTHON24:-`which python2.4`}
 PYTHON24=${PYTHON24:-/opt/python24/bin/python}
+
 PYTHON25=`which python25`
 PYTHON25=${PYTHON25:-`which python2.5`}
 PYTHON25=${PYTHON25:-/opt/python25/bin/python}
-PYTHON26=`which python26`
+
+if [ -a python26/bin/python ]
+then
+    PYTHON26=python26/bin/python
+fi
+PYTHON26=${PYTHON26:-`which python26`}
 PYTHON26=${PYTHON26:-`which python2.6`}
 PYTHON26=${PYTHON26:-/opt/python26/bin/python}
-PYTHON27=`which python27`
-PYTHON27=${PYTHON27:-`which python2.7`}
+
+if [ -a python27/bin/python ]
+then
+    PYTHON27=python27/bin/python
+fi
+PYTHON27=${PYTHON27:-`which python27`}
+PYTHON27=${PYTHON26:-`which python2.7`}
 PYTHON27=${PYTHON27:-/opt/python27/bin/python}
-PYTHON31=`which python31`
+
+if [ -a python31/bin/python ]
+then
+    PYTHON31=python31/bin/python
+fi
+PYTHON31=${PYTHON31:-`which python31`}
 PYTHON31=${PYTHON31:-`which python3.1`}
 PYTHON31=${PYTHON31:-/opt/python31/bin/python}
-PYTHON32=`which python32`
+
+if [ -a python32/bin/python ]
+then
+    PYTHON32=python32/bin/python
+fi
+PYTHON32=${PYTHON32:-`which python32`}
 PYTHON32=${PYTHON32:-`which python3.2`}
 PYTHON32=${PYTHON32:-/opt/python32/bin/python}
-PYTHON33=`which python33`
+
+if [ -a python33/bin/python ]
+then
+    PYTHON33=python33/bin/python
+fi
+PYTHON33=${PYTHON33:-`which python33`}
 PYTHON33=${PYTHON33:-`which python3.3`}
 PYTHON33=${PYTHON33:-/opt/python33/bin/python}
-PYTHON34=`which python34`
+
+if [ -a python34/bin/python ]
+then
+    PYTHON34=python34/bin/python
+fi
+PYTHON34=${PYTHON34:-`which python34`}
 PYTHON34=${PYTHON34:-`which python3.4`}
 PYTHON34=${PYTHON34:-/opt/python34/bin/python}
 
