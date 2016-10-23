c6d9e62542a124393c17b8a66de80af7424a3b21
Lennart Regebro <regebro@gmail.com>
2016-10-23 13:05:49 -0400

    cleanup

--- cronscript.sh
+++ cronscript.sh
@@ -14,7 +14,6 @@ make html
 make pdf-all
 cp -r build/html/* /var/www/python3porting.com
 TODAY=`date +%Y%m%d`
-echo TODAY
 cp build/*${TODAY}.pdf /var/www/python3porting.com/pdfs
 rm /var/www/python3porting.com/pdfs/SupportingPython3*latest.pdf
 ln -s /var/www/python3porting.com/pdfs/SupportingPython3-phone-1.0-${TODAY}.pdf /var/www/python3porting.com/pdfs/SupportingPython3-phone-1.0-latest.pdf
